f0371309d736251f23e8e153792c852356c2ff4b
fardeenrizvi92 <fardeenrizvi92@gmail.com>
2026-07-20 17:48:57 +0530

    Added programs related to majority_element concept

--- majorityelement.cpp
+++ majorityelement.cpp
@@ -30,4 +30,29 @@ public:
     }
 };
 
-//optimal
+//optimal   moore voting algo
+class Solution {  //tc=O(2n)
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq=0;
+        int ele=0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(freq==0){
+                ele = nums[i];   //after dec if freq dec then assign new(next) element to ele and then check its frequency.
+                //if that ele is in majority then that will be left and if not then second loop is to verify whether that is the only element which has high majority
+            }
+            if(nums[i]==ele){ 
+                freq++;   //if same element then freq inc and if different ele then freq dec 
+                
+            }
+            else freq--;
+        }
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ele) c++;     //count whether ont the left element is in majority or not 
+            if(c>(nums.size()/2)) return ele;
+        }
+        return 0;
+    }
+};
