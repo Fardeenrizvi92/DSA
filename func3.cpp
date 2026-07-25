607d93265b482a987687a2fd0764e6f7e6928223
fardeenrizvi92 <fardeenrizvi92@gmail.com>
2026-07-25 20:24:35 +0530

    Added programs

--- func3.cpp
+++ func3.cpp
@@ -36,3 +36,33 @@ int main(){
     cout<<i<<" ";
     }
 }
+
+
+//recursion
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n, int i = 2) {
+    if (n <= 1)
+        return false;
+
+    if (i * i > n)
+        return true;
+
+    if (n % i == 0)
+        return false;
+
+    return isPrime(n, i + 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (isPrime(n))
+        cout << "Prime Number";
+    else
+        cout << "Not Prime Number";
+
+    return 0;
+}
